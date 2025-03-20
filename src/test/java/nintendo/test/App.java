53cefa8492653a17b158ad27ee0a72bc8b41c26e
Damien <damien.rat44@gmail.com>
2025-03-20 17:37:42 +0100

    Add Class Achat and objects in main

--- src/test/java/nintendo/test/App.java
+++ src/test/java/nintendo/test/App.java
@@ -2,11 +2,13 @@ package nintendo.test;
 
 public class App {
 
-
 	public static void main(String[] args) {
-
+		Jeu leagueOfLegends = new Jeu("League of legends", pc, 0);
+		Jeu zelda = new Jeu("The Legend of Zelda: Breath of the Wild", nintendoSwitch , 59.99);
+        Jeu callOfDutyBO2 = new Jeu("Call of Duty : Black Ops 2", playStation3, 59.99);
+        Jeu pokemonSoulsilver = new Jeu("Pokémon Soulsilver", ds, 59.99);
+        Jeu superMarioGalaxy = new Jeu("Super Mario Galaxy", wii, 59.99);
 	}
-
 }
 
 
