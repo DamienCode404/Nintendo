136e66cedfdb411d8d7ccad22c69c848dd221adb
Damien <damien.rat44@gmail.com>
2025-03-21 10:00:49 +0100

    objets console + getter setter tostring Console

--- src/main/java/nintendo/Console.java
+++ src/main/java/nintendo/Console.java
@@ -6,4 +6,39 @@ public class Console {
 	protected LocalDate dateSortie;
 	protected double prix;
 	protected String name;
+	
+	public Console(LocalDate dateSortie, double prix, String name) {
+		this.dateSortie = dateSortie;
+		this.prix = prix;
+		this.name = name;
+	}
+
+	public LocalDate getDateSortie() {
+		return dateSortie;
+	}
+
+	public void setDateSortie(LocalDate dateSortie) {
+		this.dateSortie = dateSortie;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Console [dateSortie=" + dateSortie + ", prix=" + prix + ", name=" + name + "]";
+	}
 }
--- src/test/java/nintendo/test/App.java
+++ src/test/java/nintendo/test/App.java
@@ -1,18 +1,13 @@
 package nintendo.test;
 
-import java.io.Console;
+import java.time.LocalDate;
 
+import nintendo.Console;
 import nintendo.Jeu;
 
 public class App {
 
 	public static void main(String[] args) {
-
-		Jeu leagueOfLegends = new Jeu("League of legends", Console.pc, 0);
-		Jeu zelda = new Jeu("The Legend of Zelda: Breath of the Wild", Console.nintendoSwitch , 59.99);
-        Jeu callOfDutyBO2 = new Jeu("Call of Duty : Black Ops 2", Console.playStation3, 59.99);
-        Jeu pokemonSoulsilver = new Jeu("Pokémon Soulsilver", Console.ds, 59.99);
-        Jeu superMarioGalaxy = new Jeu("Super Mario Galaxy", Console.wii, 59.99);
         
         Client client1 = new Client("Malet", "Adrien");
         Client client2 = new Client("Valentin", "Alexandre");
@@ -21,6 +16,24 @@ public class App {
 
         Adresse adresse = new Adresse(1, "rue Link", "Nantes");
         Boutique boutique = new Boutique("Nintendo", adresse);
+        
+		Console pc = new Console(LocalDate(1987-10-30), 3200, "PC");
+		Console nintendoSwitch = new Console(LocalDate(2015-10-30), 200, "Nintendo Switch");
+		Console playStation3 = new Console(LocalDate(2004-10-30), 300, "playStation3");
+		Console ds = new Console(LocalDate(2000-10-30), 150, "ds");
+		Console wii = new Console(LocalDate(20010-10-30), 250, "wii");
+		
+		Jeu leagueOfLegends = new Jeu("League of legends", Console.pc, 0);
+		Jeu zelda = new Jeu("The Legend of Zelda: Breath of the Wild", Console.nintendoSwitch , 59.99);
+        Jeu callOfDutyBO2 = new Jeu("Call of Duty : Black Ops 2", Console.playStation3, 59.99);
+        Jeu pokemonSoulsilver = new Jeu("Pokémon Soulsilver", Console.ds, 59.99);
+        Jeu superMarioGalaxy = new Jeu("Super Mario Galaxy", Console.wii, 59.99);
+
+	}
+
+	private static LocalDate LocalDate(int i) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
 
